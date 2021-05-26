065588584a8367de84eab7e374e585e0e9bb393b
cololaborde <64672548+cololaborde@users.noreply.github.com>
2021-05-26 18:36:33 -0300

    Add files via upload

---
+++ rest-server-example.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "contiki.h"
+#include "contiki-net.h"
+#include "rest-engine.h"
+#include "dev/leds.h"
+
+#if PLATFORM_HAS_BUTTON
+#include "dev/button-sensor.h"
+#endif
+
+#define DEBUG 0
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]", (lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3], (lladdr)->addr[4], (lladdr)->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#define PRINTLLADDR(addr)
+#endif
+
+/*
+ * Resources to be activated need to be imported through the extern keyword.
+ * The build system automatically compiles the resources in the corresponding sub-directory.
+ */
+extern resource_t
+  res_ejer2RED,
+  res_ejer2BLUE,
+  res_ejer2GREEN,
+  res_ejer3_getPos;
+#if PLATFORM_HAS_LEDS
+extern resource_t;
+#endif
+#if PLATFORM_HAS_LIGHT
+#include "dev/light-sensor.h"
+extern resource_t res_light;
+#endif
+
+
+PROCESS(er_example_server, "Server");
+AUTOSTART_PROCESSES(&er_example_server);
+
+PROCESS_THREAD(er_example_server, ev, data)
+{
+  PROCESS_BEGIN();
+
+  PROCESS_PAUSE();
+
+  PRINTF("Starting Erbium Example Server\n");
+
+#ifdef RF_CHANNEL
+  PRINTF("RF channel: %u\n", RF_CHANNEL);
+#endif
+#ifdef IEEE802154_PANID
+  PRINTF("PAN ID: 0x%04X\n", IEEE802154_PANID);
+#endif
+
+  PRINTF("uIP buffer: %u\n", UIP_BUFSIZE);
+  PRINTF("LL header: %u\n", UIP_LLH_LEN);
+  PRINTF("IP+UDP header: %u\n", UIP_IPUDPH_LEN);
+  PRINTF("REST max chunk: %u\n", REST_MAX_CHUNK_SIZE);
+
+  /* Initialize the REST engine. */
+  rest_init_engine();
+
+  /*
+   * Bind the resources to their Uri-Path.
+   * WARNING: Activating twice only means alternate path, not two instances!
+   * All static variables are the same for each URI path.
+   */
+	
+  leds_on(LEDS_BLUE);
+
+  rest_activate_resource(&res_ejer2RED, "leds/red");
+  rest_activate_resource(&res_ejer2BLUE, "leds/blue");
+  rest_activate_resource(&res_ejer2GREEN, "leds/green");
+  rest_activate_resource(&res_ejer3_getPos, "position");
+
+
+#if PLATFORM_HAS_LIGHT
+  rest_activate_resource(&res_light, "sensors/light"); 
+  SENSORS_ACTIVATE(light_sensor);  
+#endif
+
+
+  /* Define application-specific events here. */
+  while(1) {
+    PROCESS_WAIT_EVENT();
+
+  }                             /* while (1) */
+
+  PROCESS_END();
+}
